55e8aff61a8c23a5fec911fd033f5fd3538e68c4
Verrelio C. Rizky <verrelioragna@gmail.com>
2021-01-29 14:52:39 +0700

    [Verrel] setup collection view

--- Dota 2 Directories/Features/List Heroes/ListHeroesViewController.swift
+++ Dota 2 Directories/Features/List Heroes/ListHeroesViewController.swift
@@ -25,6 +25,12 @@ class ListHeroesViewController: UIViewController {
         super.viewDidLoad()
         self.title = "Dota 2 Heroes"
     }
+    
+    private func setupCollectionView() {
+        heroesListCollectionView.dataSource = self
+        heroesListCollectionView.delegate = self
+        heroesListCollectionView.register([HeroesItemCollectionCell.self])
+    }
 
 }
 
@@ -48,5 +54,25 @@ extension ListHeroesViewController: IListHeroesView {
     
     func reloadView() {
         
+    }
+}
+
+extension ListHeroesViewController: UICollectionViewDataSource, UICollectionViewDelegate, UICollectionViewDelegateFlowLayout {
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 1
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        return UICollectionViewCell()
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let width = UIScreen.main.bounds.width - 48
+        let height = width / 256 * 184
+        return CGSize(width: width, height: height)
     }
 }
