b199ab41bf084f7249e1b00148b272819343c324
Verrelio C. Rizky <verrelioragna@gmail.com>
2021-01-30 13:50:59 +0700

    [Verrel] add sdwebimage & flex
    - SDWebImage for image cache
    - Flex for debugging helper

--- Dota 2 Directories/Core/Extensions/Extension+UIImageView.swift
+++ Dota 2 Directories/Core/Extensions/Extension+UIImageView.swift
@@ -7,24 +7,14 @@
 
 import Foundation
 import UIKit
+import SDWebImage
 
 extension UIImageView {
-    func setImage(url: URL, placeholder: UIImage?, cache: URLCache? = nil) {
-        let cache = cache ?? URLCache.shared
-        let request = URLRequest(url: url)
-        if let data = cache.cachedResponse(for: request)?.data, let image = UIImage(data: data) {
-            self.image = image
-        } else {
-            self.image = placeholder
-            URLSession.shared.dataTask(with: request, completionHandler: { (data, response, error) in
-                if let data = data, let response = response, ((response as? HTTPURLResponse)?.statusCode ?? 500) < 300, let image = UIImage(data: data) {
-                    let cachedData = CachedURLResponse(response: response, data: data)
-                    cache.storeCachedResponse(cachedData, for: request)
-                    DispatchQueue.main.async { [unowned self] in
-                        self.image = image
-                    }
-                }
-            }).resume()
+    func setImage(url: String, placeholder: UIImage?, completion: ((Error?) -> Void)? = nil) {
+        guard let newUrl = URL(string: url) else { return }
+        SDWebImageCodersManager.sharedInstance().addCoder(SDWebImageGIFCoder.shared())
+        sd_setImage(with: newUrl, placeholderImage: placeholder, options: []) { (_, error, _, _) in
+            completion?(error)
         }
     }
 }
---
+++ Podfile
@@ -0,0 +1,13 @@
+# Uncomment the next line to define a global platform for your project
+platform :ios, '10.0'
+
+use_frameworks!
+inhibit_all_warnings!
+
+target 'Dota 2 Directories' do
+
+  pod 'SDWebImage', '4.4.8'
+  pod 'SDWebImage/WebP', '4.4.8'
+  pod 'FLEX', '4.4.0', :configurations => ['Debug']
+
+end
