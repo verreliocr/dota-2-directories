5795ea468d71e72badbd671ee5f2c322cdb0132d
Verrelio C. Rizky <verrelioragna@gmail.com>
2021-01-30 14:43:19 +0700

    [Verrel] add placeholder for heroes image

--- Dota 2 Directories/Features/List Heroes/Cells/HeroesItemCollectionCell.swift
+++ Dota 2 Directories/Features/List Heroes/Cells/HeroesItemCollectionCell.swift
@@ -9,16 +9,22 @@ import UIKit
 
 class HeroesItemCollectionCell: UICollectionViewCell {
 
+    @IBOutlet weak var placeholderView: UIView!
+    @IBOutlet weak var loadingIndicator: UIActivityIndicatorView!
     @IBOutlet weak var heroesImageView: UIImageView!
     @IBOutlet weak var heroesNameLabel: UILabel!
     
     override func awakeFromNib() {
         super.awakeFromNib()
+        loadingIndicator.startAnimating()
     }
     
     func bind(imgUrl: String, name: String) {
-        if let url = URL(string: "\(baseURL)\(imgUrl)") {
-            heroesImageView.setImage(url: url, placeholder: nil)
+        self.heroesImageView.isHidden = true
+        self.placeholderView.isHidden = false
+        heroesImageView.setImage(url: "\(baseURL)\(imgUrl)", placeholder: nil) { [unowned self] _ in
+            self.heroesImageView.isHidden = false
+            self.placeholderView.isHidden = true
         }
         heroesNameLabel.text = name
     }
